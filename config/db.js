183bdec0c9de3068ca32c93805761732a35ff6cf
xmx134 <977784602@qq.com>
2024-09-29 20:19:01 +0800

    初始化项目，添加mongodb连接，输出template模板接口

---
+++ config/db.js
@@ -0,0 +1,10 @@
+'use strict'
+
+/* MONGODB */
+const mongodbUrl = 'mongodb://xmx:123456@127.0.0.1:27017/xmx-cli'
+const mongodbDBName = 'xmx-cli'
+
+module.exports = {
+  mongodbUrl,
+  mongodbDBName
+}
